3b92ca9b3dda9b876749e2eafcfb450c97b28202
edumorris <88796134+morrisedu@users.noreply.github.com>
2022-05-23 11:00:30 +0300

    Base UI setup

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.http.response import JsonResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+def eventsView(request):
+    context = {
+        "title": "etikiti - Events"
+    }
+    
+    return render(request, "events.html", context)
